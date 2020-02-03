1397b5eb3433213cad2f1f22f677c8825f282118
Vladislav <tm_vladislav@mail.ru>
2020-02-04 00:08:55 +0200

    XMLHttpRequest

--- XMLHttpRequest_jquery/api/deleteFile.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $array_name_file = "../../" . $_POST["nameFile"] . ".html";
-
-    $file_del = file_exists($array_name_file) ? unlink($array_name_file) : false;
-
-    echo $file_del ? "Файл удалён" : "Файл не найден";
-  
-?>
